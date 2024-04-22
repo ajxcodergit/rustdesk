aebc036b734268985332a45b2d0bab66efce699c
Andre <63238049+ajxcodergit@users.noreply.github.com>
2024-04-22 20:00:36 -0300

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,8 +101,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["http://24.199.119.235"];
-pub const PUBLIC_RS_PUB_KEY: &str = "Hm3Bg7xShCCD86NP5SSThTBOtIHzbezlYXGt3iX59Vo=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["http://ajxremoto.ddns.net"];
+pub const PUBLIC_RS_PUB_KEY: &str = "UGva2g0279gYAJDYlmACY202IhKWiDY82x+obiw04dU=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
